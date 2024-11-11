43a6b0c63f8086cebb07d0bbf2ca47277f24be2e
Rabotyaga1155 <cool.poryadin2014@yandex.ru>
2024-11-11 17:58:30 +0500

    fix: remade Input.tsx

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,37 +1,40 @@
-import { useForm } from 'react-hook-form'
-import { Button } from '../ui/buttons/Button.tsx'
-import { Input } from '../ui/inputs/Input.tsx'
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Button } from "../ui/buttons/Button.tsx";
+import { Input } from "../ui/inputs/Input.tsx";
 
 export const Login = () => {
-	const {register, handleSubmit, formState: {errors}} = useForm()
-	
-	const onSubmit = (data: any) => {
-			console.log(data);
-	}
-	
-	return (
-		<form onSubmit={handleSubmit(onSubmit)} className={'space-y-6 w-96'}>
-			<h2 className={'text-3xl font-bold text-black text-center'}>Log In</h2>
-			
-			<Input
-				label={'Email'}
-				type={'email'}
-				placeholder={'Введите email'}
-				{...register('email', {required: 'Email is required'})}
-				error={errors.email?.message}
-			
-			/>
-			<Input
-				label={'Password'}
-				type={'password'}
-				placeholder={'Введите пароль'}
-				{...register('password', { required: 'Password is required'})}
-				error={errors.password?.message}
-			/>
-			
-			<Button className={'mt-2 w-full flex justify-center'} type={'submit'}>Log In</Button>
-			
-			
-		</form>
-	)
-}
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit: SubmitHandler<any> = (data: any) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className={"space-y-6 w-96"}>
+      <h2 className={"text-3xl font-bold text-black text-center"}>Log In</h2>
+
+      <Input
+        label={"dasda"}
+        type={"email"}
+        placeholder={"Введите email"}
+        register={register("Email", { required: "Email required" })}
+        error={errors.email?.message}
+      />
+      <Input
+        label={"Password"}
+        type={"password"}
+        placeholder={"Введите пароль"}
+        register={register("password", { required: "password required" })}
+        error={errors.password?.message}
+      />
+
+      <Button className={"mt-2 w-full flex justify-center"} type={"submit"}>
+        Log In
+      </Button>
+    </form>
+  );
+};
--- src/components/ui/inputs/Input.tsx
+++ src/components/ui/inputs/Input.tsx
@@ -1,23 +1,34 @@
-import clsx from 'clsx'
-import type { InputHTMLAttributes } from 'react'
+import clsx from "clsx";
+import type { FC, InputHTMLAttributes } from "react";
 
 interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
-	error?: string;
+  error?: string;
+  label: string;
+  register: any;
 }
 
-export const Input = ({children, error, ...props}): IInputProps => {
-	return (
-		<div className={'flex flex-col space-y-2'}>
-			{props.label && <label className={'font-semibold text-black text-base '}>{props.label}</label>}
-			<input
-				className={clsx(
-					'px-4 py-2 border rounded-lg focus:outline-none',
-					'border-gray-300 focus:border-pink',
-					error ? 'border-red-500' : ''
-				)}
-				{...props}
-			/>
-			{error && <span className={'text-red text-sm'}>{error}</span>}
-		</div>
-	)
-}
+export const Input: FC<IInputProps> = ({
+  children,
+  error,
+  register,
+  label,
+  ...props
+}) => {
+  return (
+    <div className={"flex flex-col space-y-2"}>
+      {label && (
+        <label className={"font-semibold text-black text-base "}>{label}</label>
+      )}
+      <input
+        {...register}
+        {...props}
+        className={clsx(
+          "px-4 py-2 border rounded-lg focus:outline-none",
+          "border-gray-300 focus:border-pink",
+          error ? "border-red-500" : "",
+        )}
+      />
+      {error && <span className={"text-red text-sm"}>{error}</span>}
+    </div>
+  );
+};
